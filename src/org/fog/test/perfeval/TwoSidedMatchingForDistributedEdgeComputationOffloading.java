0bede2ed76e5b7d3d59e8778ab36fb67b32d5cea
Vignesh Srinivasan <vignesh1722@gmail.com>
2022-04-04 22:36:05 +0530

    created TwoSidedMatchingForDistributedEdgeComputationOffloading

---
+++ src/org/fog/test/perfeval/TwoSidedMatchingForDistributedEdgeComputationOffloading.java
@@ -0,0 +1,170 @@
+package org.fog.test.perfeval;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.Pe;
+import org.cloudbus.cloudsim.Storage;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.power.PowerHost;
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
+import org.cloudbus.cloudsim.sdn.overbooking.BwProvisionerOverbooking;
+import org.cloudbus.cloudsim.sdn.overbooking.PeProvisionerOverbooking;
+import org.fog.application.AppEdge;
+import org.fog.application.AppLoop;
+import org.fog.application.Application;
+import org.fog.application.selectivity.FractionalSelectivity;
+import org.fog.entities.Actuator;
+import org.fog.entities.FogBroker;
+import org.fog.entities.FogDevice;
+import org.fog.entities.FogDeviceCharacteristics;
+import org.fog.entities.Sensor;
+import org.fog.entities.Tuple;
+import org.fog.placement.Controller;
+import org.fog.placement.ModuleMapping;
+import org.fog.placement.ModulePlacementEdgewards;
+import org.fog.placement.ModulePlacementMapping;
+import org.fog.policy.AppModuleAllocationPolicy;
+import org.fog.scheduler.StreamOperatorScheduler;
+import org.fog.utils.FogLinearPowerModel;
+import org.fog.utils.FogUtils;
+import org.fog.utils.TimeKeeper;
+import org.fog.utils.distribution.DeterministicDistribution;
+
+public class TwoSidedMatchingForDistributedEdgeComputationOffloading {
+
+    static List<FogDevice> fogDevices = new ArrayList<FogDevice>();
+    static List<FogDevice> mobiles = new ArrayList<FogDevice>();
+
+    static int numAP = 3;
+
+    public static void main(String[] args) {
+        Log.printLine("Starting Two-Sided Matching Approach for Distributed Edge Computation Ofﬂoading...");
+
+        try {
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            Log.printLine("Unwanted errors happen");
+        }
+    }
+
+    private static FogDevice addMobile(String id, int parentId){
+        FogDevice mobile = createFogDevice("m-"+id, 1000, 1000, 10000, 270, 3, 0, 87.53, 82.44);
+        mobile.setParentId(parentId);
+        mobiles.add(mobile);
+		/*Sensor eegSensor = new Sensor("s-"+id, "EEG", userId, appId, new DeterministicDistribution(EEG_TRANSMISSION_TIME)); // inter-transmission time of EEG sensor follows a deterministic distribution
+		sensors.add(eegSensor);
+		Actuator display = new Actuator("a-"+id, userId, appId, "DISPLAY");
+		actuators.add(display);
+		eegSensor.setGatewayDeviceId(mobile.getId());
+		eegSensor.setLatency(6.0);  // latency of connection between EEG sensors and the parent Smartphone is 6 ms
+		display.setGatewayDeviceId(mobile.getId());
+		display.setLatency(1.0);  // latency of connection between Display actuator and the parent Smartphone is 1 ms
+*/		return mobile;
+    }
+
+    /**
+     * Creates a vanilla fog device
+     * @param nodeName name of the device to be used in simulation
+     * @param mips MIPS
+     * @param ram RAM
+     * @param upBw uplink bandwidth
+     * @param downBw downlink bandwidth
+     * @param level hierarchy level of the device
+     * @param ratePerMips cost rate per MIPS used
+     * @param busyPower
+     * @param idlePower
+     * @return
+     */
+    private static FogDevice createFogDevice(String nodeName, long mips,
+                                             int ram, long upBw, long downBw, int level, double ratePerMips, double busyPower, double idlePower) {
+
+        List<Pe> peList = new ArrayList<Pe>();
+
+        // 3. Create PEs and add these into a list.
+        peList.add(new Pe(0, new PeProvisionerOverbooking(mips))); // need to store Pe id and MIPS Rating
+
+        int hostId = FogUtils.generateEntityId();
+        long storage = 1000000; // host storage
+        int bw = 10000;
+
+        PowerHost host = new PowerHost(
+                hostId,
+                new RamProvisionerSimple(ram),
+                new BwProvisionerOverbooking(bw),
+                storage,
+                peList,
+                new StreamOperatorScheduler(peList),
+                new FogLinearPowerModel(busyPower, idlePower)
+        );
+
+        List<Host> hostList = new ArrayList<Host>();
+        hostList.add(host);
+
+        String arch = "x86"; // system architecture
+        String os = "Linux"; // operating system
+        String vmm = "Xen";
+        double time_zone = 10.0; // time zone this resource located
+        double cost = 3.0; // the cost of using processing in this resource
+        double costPerMem = 0.05; // the cost of using memory in this resource
+        double costPerStorage = 0.001; // the cost of using storage in this
+        // resource
+        double costPerBw = 0.0; // the cost of using bw in this resource
+        LinkedList<Storage> storageList = new LinkedList<Storage>(); // we are not adding SAN
+        // devices by now
+
+        FogDeviceCharacteristics characteristics = new FogDeviceCharacteristics(
+                arch, os, vmm, host, time_zone, cost, costPerMem,
+                costPerStorage, costPerBw);
+
+        FogDevice fogdevice = null;
+        try {
+            fogdevice = new FogDevice(nodeName, characteristics,
+                    new AppModuleAllocationPolicy(hostList), storageList, 10, upBw, downBw, 0, ratePerMips);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        fogdevice.setLevel(level);
+        return fogdevice;
+    }
+
+    /**
+     * Function to create the Distributed Edge Computing Offloading application.
+     * @param appId unique identifier of the application
+     * @param userId identifier of the user of the application
+     * @return
+     */
+    @SuppressWarnings({"serial" })
+    private static Application createApplication(String appId, int userId){
+
+        Application application = Application.createApplication(appId, userId);
+        /*
+         * Adding modules (vertices) to the application model (directed graph)
+         */
+        application.addAppModule("object_detector", 10);
+
+        /*
+         * Connecting the application modules (vertices) in the application model (directed graph) with edges
+         */
+        application.addAppEdge("CAMERA", "motion_detector", 1000, 20000, "CAMERA", Tuple.UP, AppEdge.SENSOR); // adding edge from CAMERA (sensor) to Motion Detector module carrying tuples of type CAMERA
+
+        /*
+         * Defining the input-output relationships (represented by selectivity) of the application modules.
+         */
+        application.addTupleMapping("motion_detector", "CAMERA", "MOTION_VIDEO_STREAM", new FractionalSelectivity(1.0)); // 1.0 tuples of type MOTION_VIDEO_STREAM are emitted by Motion Detector module per incoming tuple of type CAMERA
+
+        /*
+         * Defining application loops (maybe incomplete loops) to monitor the latency of.
+         * Here, we add two loops for monitoring : Motion Detector -> Object Detector -> Object Tracker and Object Tracker -> PTZ Control
+         */
+        final AppLoop loop1 = new AppLoop(new ArrayList<String>(){{add("motion_detector");add("object_detector");add("object_tracker");}});
+
+        return application;
+    }
+}
